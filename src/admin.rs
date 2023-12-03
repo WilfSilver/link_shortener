28b46f003a1585758dd66d4ffa9bb32d78dc1c35
Wilf Silver <git@wilfsilver.co.uk>
2023-12-03 18:42:35 +0000

    Add ability to assign prefixes to users

--- src/admin.rs
+++ src/admin.rs
@@ -1,17 +1,36 @@
+use diesel_async::AsyncConnection;
 use rocket::response::Redirect;
+use rocket_db_pools::Connection;
 use rocket_dyn_templates::{context, Template};
 
-use crate::auth::User;
+use crate::api::API_LOCAL;
+use crate::auth::{self, User};
+use crate::database::{Db, PrefixLink};
 use crate::utils::random_colour;
 
-use crate::api::API_LOCAL;
-use crate::auth;
-
 #[get("/")]
-pub fn index(_user: User) -> Template {
+pub async fn index(mut db: Connection<Db>, user: User) -> Template {
+    println!("User: {}", user.id);
+    let prefixes: Vec<PrefixLink> = db
+        .transaction(|conn| {
+            Box::pin(async move {
+                Ok::<_, diesel::result::Error>(PrefixLink::get_all(conn, &user.id).await)
+            })
+        })
+        .await
+        .unwrap_or_default();
+
+    println!("Prefixes: {}", prefixes.len());
+
     Template::render(
         "shortener",
-        context! { api: API_LOCAL, colour: random_colour(), name: "Home" },
+        context! {
+            api: API_LOCAL,
+            colour: random_colour(),
+            allow_custom_name: !prefixes.is_empty(),
+            prefixes: prefixes,
+            name: "Home",
+        },
     )
 }
 
